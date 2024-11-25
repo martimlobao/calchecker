0b1e6fcaaec337f839e8418c2b28a58f8b838a0d
Martim Lobao <dev@lobao.io>
2024-11-25 11:36:15 +0000

    feat: Check calendary summaries - compare event summaries and include the diff in the logs

--- calchecker/__main__.py
+++ calchecker/__main__.py
@@ -82,16 +82,36 @@ def main(url: str, calendar_state: str, key: str) -> str:
 
     previous_events = load_state(calendar_state, key)
     current_events = parse_calendar(calendar_data)
-    logs = []
+    log_dict: dict[str, list[str]] = {
+        "new_events": [],
+        "deleted_events": [],
+        "updated_events": [],
+    }
 
-    if new_events := [event for event in current_events if event not in previous_events]:
+    if new := [event for event in current_events if event not in previous_events]:
+        log_dict["new_events"].extend(format_event(current_events[uid]) for uid in new)
+    if deleted := [event for event in previous_events if event not in current_events]:
+        log_dict["deleted_events"].extend(format_event(previous_events[uid]) for uid in deleted)
+    for uid in previous_events.keys() & current_events.keys():
+        old_summary = str(previous_events[uid]["SUMMARY"])
+        new_summary = str(current_events[uid]["SUMMARY"])
+        if old_summary != new_summary:
+            log_dict["updated_events"].append(f"{uid}: {old_summary} -> {new_summary}")
+
+    logs = []
+    if log_dict["new_events"]:
         logs.append("New events added")
-        logs.extend(format_event(current_events[uid]) for uid in new_events)
-    if deleted_events := [event for event in previous_events if event not in current_events]:
+        logs.extend(log_dict["new_events"])
+    if log_dict["deleted_events"]:
         if logs:
             logs.append("")
         logs.append("Events deleted")
-        logs.extend(format_event(previous_events[uid]) for uid in deleted_events)
+        logs.extend(log_dict["deleted_events"])
+    if log_dict["updated_events"]:
+        if logs:
+            logs.append("")
+        logs.append("Events updated")
+        logs.extend(log_dict["updated_events"])
 
     save_state(calendar_state, current_events, key)
     return "\n".join(logs)
